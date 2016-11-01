6d8ce401aa6199c06c1fb43d52f46696cf298eaa
Sonny <son.froz.arc@gmail.com>
2016-11-02 04:36:02 +0700

    Added DDA algorithm and fixed Bresenham

--- LineDraw/LineDraw.cpp
+++ LineDraw/LineDraw.cpp
@@ -8,12 +8,19 @@
 #include <GL\glut.h>
 #include <iostream>
 
-using namespace std;
 
+using namespace std;
+int mode = 0;
 //identifier fungsi
+void function();
+void DD();
+void Bresenham();
+void Circle();
+
 void init();
 void display();
-void function();
+
+
 
 //  posisi window di layar
 int window_x;
@@ -24,10 +31,12 @@ int window_width = 480;
 int window_height = 480;
 
 //  judul window
-char *judul_window = "Algoritma Bresenham";
+char *judul_window = "Garis";
 
 void main(int argc, char **argv)
 {
+	cout << "Mode : ";
+	cin >> mode;
 	//  inisialisasi GLUT (OpenGL Utility Toolkit)
 	glutInit(&argc, argv);
 	// set posisi window supaya berada di tengah 
@@ -43,6 +52,19 @@ void main(int argc, char **argv)
 	init(); //jalankan fungsi init 
 	glutDisplayFunc(display); //  set fungsi display
 	glutMainLoop(); // set loop pemrosesan GLUT
+}
+
+void function() {
+	switch (mode)
+	{
+	case 1:		DD();
+		break;
+	case 2:		Bresenham();
+		break;
+	case 3:		Circle();
+		break;
+	default:	break;
+	}
 }
 
 void init()
@@ -61,61 +83,127 @@ void display()
 	glutSwapBuffers(); //swap buffer 
 }
 
-void function() {	//dummy, mau diganti
-	int x1, y1, x2, y2, x, y, dx, dy, xend, p, duady, duadydx;
-	//tentukan titik awal dan akhir
-	x1 = 10;
-	y1 = 10;
-	x2 = 500;
-	y2 = 400;
-	x = x1;
-	y = y1;
+void DD() {
+	int x1, x2, y1, y2, dx, dy, step;
+	float x,y,x_inc,y_inc;
 
-	//hitung dx dan dy
-	dx = abs(x2 - x1);
-	dy = abs(y2 - y1);
+	x2 = 10;
+	y2 = 10;
+	x1 = 500;
+	y1 = 400;
+	x = (float)x1;
+	y = (float)y1;
 
-	//hitung p 
-	p = 2 * dy - dx;
-	duady = 2 * dy;
-	duadydx = 2 * (dy - dx);
-
-	//tentukan titik awal dan akhir
-	if (x1 > x2) {
-		x = x2;
-		y = y2;
-		xend = x1;
+	dx = x2 - x1;
+	dy = y2 - y1;
+	if (abs(dx) > abs(dy)) {
+		step = abs(dx);
 	}
-	else
-	{
-		x = x1;
-		y = y1;
-		xend = x2;
+	else {
+		step = abs(dy);
 	}
+
+	x_inc = (float)dx / (float)step;
+	y_inc = (float)dy / (float)step;
 
 	//gambar titik awal
 	glBegin(GL_POINTS);
-	glVertex2i(x, y);
+	glVertex2i((int)x, (int)y);
 
 	//perulangan untuk menggambar titik-titik 
-	while (x < xend) {
-		x++;
-		if (p < 0) {
-			p += duady;
-		}
-		else
-		{
-			if (y1 > y2) {
-				y--;
-			}
-			else y++;
-
-			p += duadydx;
-		}
-		glVertex2i(x, y);
+	while (x != (float)x2 && y!=(float)y2) {
+		x += x_inc;
+		y += y_inc;
+		glVertex2i((int)x, (int)y);
 	}
 
 	glEnd();
 	glFlush();
 }
 
+void Bresenham() {	//dummy, mau diganti
+	int x1, y1, x2, y2, dx, dy, x, y, p,d2,dydx2;
+	bool m1 = false;
+
+	x2 = 10;
+	y2 = 10;
+	x1 = 500;
+	y1 = 400;
+	x = x1;
+	y = y1;
+
+	dx = abs(x2 - x1);
+	dy = abs(y2 - y1);
+	if (dx<dy) {
+		m1 = true;
+		d2 = 2 * dx;
+		dydx2 = 2*(dx-dy);
+		p = d2 - dy;
+	}
+	else {
+		d2 = 2 * dy;
+		dydx2 = 2 * (dy - dx);
+		p = d2 - dx;
+	}
+
+
+	//gambar titik awal
+	glBegin(GL_POINTS);
+	glVertex2i(x, y);
+
+	//perulangan untuk menggambar titik-titik 
+	if (m1) {
+		while (x != x2 && y != y2) {
+			if (y1 > y2) {
+				y--;
+			}
+			else y++;
+			if (p < 0) {
+				p += d2;
+			}
+			else
+			{
+
+				if (x1>x2) {
+					x--;
+				}
+				else {
+					x++;
+				}
+
+				p += dydx2;
+			}
+			glVertex2i(x, y);
+		}
+	}
+	else {
+		while (x != x2 && y != y2) {
+			if (x1 > x2) {
+				x--;
+			}
+			else {
+				x++;
+			}
+			if (p < 0) {
+				p += d2;
+			}
+			else
+			{
+				if (y1 > y2) {
+					y--;
+				}
+				else y++;
+
+				p += dydx2;
+			}
+			glVertex2i(x, y);
+		}
+	}
+
+	glEnd();
+	glFlush();
+}
+
+void Circle() {
+
+}
